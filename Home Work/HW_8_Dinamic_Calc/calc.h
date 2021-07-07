dbfef1c3f92a5a68db8024166e77fd7281951385
IVA <contact@iva-g.cc>
2021-07-07 12:38:24 +0300

    add HW_8

---
+++ Home Work/HW_8_Dinamic_Calc/calc.h
@@ -0,0 +1,10 @@
+#ifndef CALC_H_INCLUDED
+#define CALC_H_INCLUDED
+
+float add(float, float);
+float minus(float, float);
+float multiply(float, float);
+float divide(float, float);
+ 
+#endif // CALC_H_INCLUDED
+
---
+++ Home Work/HW_8_Dinamic_Calc/getMenu.h
@@ -0,0 +1,4 @@
+#ifndef GETMENU_H
+#define GETMENU_H
+    void get_menu();
+#endif
