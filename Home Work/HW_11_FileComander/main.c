77b62363e3590e34cdd5a54deb51188d9e46904a
IVA <contact@iva-g.cc>
2021-07-24 15:08:49 +0300

    add File List

--- Home Work/HW_11_FileComander/main.c
+++ Home Work/HW_11_FileComander/main.c
@@ -8,6 +8,7 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <dirent.h>
 
 volatile sig_atomic_t signal_status = 0;
 
@@ -81,6 +82,33 @@ void print_right_win(struct winsize size, int mid, WINDOW **border_right_win, WI
     }
 }
 
+int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+void print_files_list(WINDOW *win, struct dirent **file_list, int n, int curr_row,int up_bnd, int low_bnd)
+{
+    werase(win);
+
+    for(int i = up_bnd; i < min(n, low_bnd); i++)
+    {
+        wmove(win, i - up_bnd, 0);
+        if(i == curr_row)
+        {
+            wattron(win, COLOR_PAIR(1));
+            wprintw(win, "%s", file_list[i]->d_name);
+            wattron(win, COLOR_PAIR(2));
+        }
+        else 
+        {
+            wprintw(win, "%s", file_list[i]->d_name);
+        }
+    }
+    wrefresh(win);
+
+}
+
 int main(void)
 {
     init();
@@ -106,6 +134,62 @@ int main(void)
     WINDOW *right_win;
     print_left_win(size, mid, &border_left_win, &left_win);
     print_right_win(size, mid, &border_right_win, &right_win);
+    
+    //Файлы для левого окна
+    struct dirent **file_list_lw;
+    int n_lw = 0;
+    int curr_row_lw = 0;
+    int up_bnd_lw = 0;
+    int low_bnd_lw = size.ws_row - 2;
+    char *curr_dir_lw = (char *)malloc(sizeof(char) * 2);
+    strcpy(curr_dir_lw, ".");
+    n_lw = scandir(curr_dir_lw, &file_list_lw, 0, alphasort);
+    if(n_lw < 0)
+    {
+        finish();
+        exit(-5);
+    }
+    else
+    {
+        print_files_list
+        (
+         left_win, 
+         file_list_lw, 
+         n_lw,               
+         curr_row_lw, 
+         up_bnd_lw,  
+         low_bnd_lw
+        );
+    }
+    
+    //Файлы для правого окна
+    struct dirent **file_list_rw;
+    int n_rw = 0;
+    int curr_row_rw = 0;
+    int up_bnd_rw = 0;
+    int low_bnd_rw = size.ws_row - 2;
+    char *curr_dir_rw = (char *)malloc(sizeof(char) * 2);
+    strcpy(curr_dir_rw, ".");
+    n_rw = scandir(curr_dir_rw, &file_list_rw, 0, alphasort);
+    if(n_rw < 0)
+    {
+        finish();
+        exit(-6);
+    }
+    else
+    {
+        print_files_list
+        (
+         right_win, 
+         file_list_rw, 
+         n_rw,               
+         curr_row_rw, 
+         up_bnd_rw,  
+         low_bnd_rw
+        );
+ 
+    }
+
 
     napms(3000);
     
