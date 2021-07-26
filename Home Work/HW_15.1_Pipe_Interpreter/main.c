946ff72461a7c06318a255edd6b7ddcfdf897f2f
IVA <contact@iva-g.cc>
2021-07-26 19:25:54 +0300

    add dups

--- Home Work/HW_15.1_Pipe_Interpreter/main.c
+++ Home Work/HW_15.1_Pipe_Interpreter/main.c
@@ -10,6 +10,7 @@
 #define MAX_LEN 128
 
 void print_start_image();
+
 typedef enum {false, true} bool;
 
 int main(void)
@@ -17,12 +18,14 @@ int main(void)
     char dir[PATH_MAX];
     char comm[ARG_MAX];
     char *curr_char; 
-    char *args_1;    
-    char *args_2;
+    char *args_1[MAX_LEN];    
+    char *args_2[MAX_LEN];
     int args_flag = false;
     char *pipe_flag = NULL;
     short shift = 0;
-    int i = 0;
+    int args_count = 1;
+    int fd[2];
+    pid_t pid[2];
     struct passwd *userinfo;
     uid_t userid;
     userinfo = getpwuid(userid);
@@ -33,12 +36,12 @@ int main(void)
     {  
         if (getcwd(dir, PATH_MAX) == NULL)
         {
-            fprintf(stderr, "error: getcwd fail");
+            fprintf(stderr, "error: getcwd fail\n");
             exit(EXIT_FAILURE);
         }
         else if (userinfo == NULL) 
         {
-            fprintf(stderr, "error: userinfo fail");
+            fprintf(stderr, "error: userinfo fail\n");
             exit(EXIT_FAILURE);
         }
         else
@@ -57,34 +60,110 @@ int main(void)
         
         if(strcmp(comm, "quit") == 0)
         {
-            printf("bye\n");
-            exit(0);
+            exit(EXIT_SUCCESS);
         }
-        
-        
-        args_1 = (char*)malloc(strlen(comm));
              
-        if((args_1 = strtok(comm, "|")) == NULL)
+        if((*args_1 = strtok(comm, "|")) == NULL)
         {
-            fprintf(stderr, "error: string cannot be split");
+            fprintf(stderr, "error: string cannot be split\n");
             exit(EXIT_FAILURE);
         }
-        else if((pipe_flag = strtok(NULL, "|")) != NULL)
+        if((pipe_flag = strtok(NULL, "|")) != NULL)
         {
             args_flag = true;
             while(pipe_flag[shift] == ' ')
             {
                 shift++;
             }
-            args_2 = (char*)malloc(strlen(pipe_flag)); 
-            args_2 = pipe_flag + shift;
+            *args_2 = pipe_flag + shift;
         }
-        printf("%s\n", args_1);
-        printf("%s\n", args_2);
+         
+        for(curr_char = *args_1; *curr_char != 0; curr_char++)
+        {
+            if((*curr_char == ' ') && (((*curr_char) + 1) != ' '))
+            {
+                *curr_char= '\0';
+                args_1[args_count] = curr_char + 1;
+                args_count++;
+            } 
+        }
+
+        if(args_flag == true)
+        {
+            args_1[args_count - 1] = NULL;
+            args_count = 1;
+            
+            for(curr_char = *args_2; *curr_char != 0; curr_char++)
+            {
+                if((*curr_char == ' ') && (((*curr_char) + 1) != ' '))
+                {
+                    *curr_char = '\0';
+                    args_2[args_count] = curr_char + 1;
+                    args_count++;
+                }
+            }
+            pipe(fd);
+        }
         
+        switch (pid[0] = fork()) 
+        {
+            case -1:
+            {
+                fprintf(stderr, "error: fork fail\n");
+                exit(EXIT_FAILURE);
+            }
+            case 0:
+            {
+                if(args_flag == true)
+                {
+                    dup2(fd[1], 1);
+                    close(fd[0]);
+                    close(fd[1]);
+                }
+
+                if(execvp(*args_1, args_1) == -1)
+                {
+                    fprintf(stderr, "error: execvp fail\n");
+                    exit(EXIT_FAILURE);
+                }
+                
+            }
+        }
+        
+        if(args_flag == true)
+        {
+            switch (pid[1] = fork()) 
+            {
+                case -1:
+                {
+                    fprintf(stderr, "error: fork2 fail\n");
+                    exit(EXIT_FAILURE);
+                }
+                case 0:
+                {
+                    dup2(fd[0], 0);
+                    close(fd[0]);
+                    close(fd[1]);
+                }
+
+                if(execvp(*args_2, args_2) == -1)
+                {
+                    fprintf(stderr, "error: execvp2:while fail\n");
+                    exit(EXIT_FAILURE);
+                }
+                
+            }
+        }
+
+        waitpid(pid[0], NULL, WUNTRACED);
+        if(args_flag == true)
+        {
+            waitpid(pid[1], NULL, WNOHANG);
+            close(fd[0]);
+            close(fd[1]);
+        }
     }
 
-       
     return 0;
 }
     
