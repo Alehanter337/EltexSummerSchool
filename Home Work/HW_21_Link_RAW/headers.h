70bdf14d1fe7f8a5094e718ed66b5a4ea1d3b3c7
IVA <contact@iva-g.cc>
2021-08-12 17:45:19 +0300

    add headers.h

---
+++ Home Work/HW_21_Link_RAW/headers.h
@@ -0,0 +1,26 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/udp.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+
+#define handle_error(msg) \
+        do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+#define ERROR -1
+
+#define SERVER_PORT 7331
+
+#define CLIENT_PORT 7441
+
+#define PACKET_SIZE 256
+
+#endif
