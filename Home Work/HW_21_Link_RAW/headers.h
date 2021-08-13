53fbbaeb15d2edd27145f32dd1f4e06b6fbebbb8
IVA <contact@iva-g.cc>
2021-08-13 08:29:53 +0300

    add includes

--- Home Work/HW_21_Link_RAW/headers.h
+++ Home Work/HW_21_Link_RAW/headers.h
@@ -10,7 +10,9 @@
 #include <netinet/in.h>
 #include <netinet/udp.h>
 #include <netinet/ip.h>
+#include <netinet/if_ether.h>
 #include <arpa/inet.h>
+#include <net/if.h>
 
 #define handle_error(msg) \
         do { perror(msg); exit(EXIT_FAILURE); } while (0)
@@ -22,5 +24,9 @@
 #define CLIENT_PORT 7441
 
 #define PACKET_SIZE 256
+
+#define SERVER_IP "192.168.0.2"
+
+#define CLIENT_IP "192.168.0.3"
 
 #endif
